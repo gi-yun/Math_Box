02a5579b589ea570d578a8d723561307f7e74fe2
giyun <kbb9554@gmail.com>
2024-07-25 20:31:37 +0900

    reset

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#수정1
+def add(a, b):
+  return a+b
+def subtract(a, b):
+  return a-b
+def muliply(a,b);
+  return a*b
+def divide_new(a, b):
+  return a/b
---
+++ numpy/numpy/_typing/_scalars.py
@@ -0,0 +1,34 @@
+from typing import Any, TypeAlias, Union
+
+import numpy as np
+
+# NOTE: `_StrLike_co` and `_BytesLike_co` are pointless, as `np.str_` and
+# `np.bytes_` are already subclasses of their builtin counterpart
+
+_CharLike_co: TypeAlias = Union[str, bytes]
+
+# The 6 `<X>Like_co` type-aliases below represent all scalars that can be
+# coerced into `<X>` (with the casting rule `same_kind`)
+_BoolLike_co: TypeAlias = Union[bool, np.bool]
+_UIntLike_co: TypeAlias = Union[_BoolLike_co, np.unsignedinteger[Any]]
+_IntLike_co: TypeAlias = Union[_BoolLike_co, int, np.integer[Any]]
+_FloatLike_co: TypeAlias = Union[_IntLike_co, float, np.floating[Any]]
+_ComplexLike_co: TypeAlias = Union[
+    _FloatLike_co,
+    complex,
+    np.complexfloating[Any, Any],
+]
+_TD64Like_co: TypeAlias = Union[_IntLike_co, np.timedelta64]
+
+_NumberLike_co: TypeAlias = Union[int, float, complex, np.number[Any], np.bool]
+_ScalarLike_co: TypeAlias = Union[
+    int,
+    float,
+    complex,
+    str,
+    bytes,
+    np.generic,
+]
+
+# `_VoidLike_co` is technically not a scalar, but it's close enough
+_VoidLike_co: TypeAlias = Union[tuple[Any, ...], np.void]
